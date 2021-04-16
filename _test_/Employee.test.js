988f29649483ea76aeee00e775b7bb86717997d1
Adam Alcantara <adamjalcantara@gmail.com>
2021-04-16 18:52:21 -0400

    Added questions

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,8 +1,13 @@
-const { TestScheduler } = require("@jest/core");
+// const { TestScheduler } = require("@jest/core");
 const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employees");
 
-TestScheduler("Can Instantiate Employee", () => {
+test ("Can Instantiate Employee", () => {
     const employee = new Employee();
     expect(typeof(employee)).toBe("object");
+})
+
+test ("Can set name through constructor arguments", () => {
+    const employee = new Employee("Patrick");
+    expect(employee.name).toBe("Patrick");
 })
--- index.js
+++ index.js
@@ -1,6 +1,245 @@
-const Employees = require("./lib/Employees");
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
-const inquire = require("inquirer");
+const inquirer = require("inquirer");
+const path = require("path");
 const fs = require("fs");
+
+menu = () => {
+    createManager = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the manager's name",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a name."
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the team manager's id?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an ID."
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the team manager's email?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an email."
+                }
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the team manager's office number?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a number."
+                }
+            },
+
+        ]).then((data) => {
+            const manager = new Manager(
+                data.managerName, 
+                data.managerId,
+                data.managerEmail,
+                data.managerOfficeNumber
+                );
+            console.log(manager);
+            teamArray.push(manager)
+            createTeam();
+        });
+    }
+
+    createEmployee = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the employee's name",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a name."
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the employee's id?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an ID."
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an email."
+                }
+            },
+
+        ]).then((data) => {
+            const employee = new Employee(
+                data.employeeName, 
+                data.employeeId,
+                data.employeeEmail,
+                );
+            console.log(employee);
+            teamArray.push(employee)
+            createTeam();
+        });
+    }
+    
+    createIntern = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the intern's name",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a name."
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the intern's id?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an ID."
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an email."
+                }
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the intern's school?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a school."
+                }
+            },
+
+        ]).then((data) => {
+            const intern = new intern(
+                data.internName, 
+                data.internId,
+                data.internEmail,
+                data.internSchool,
+                );
+            console.log(intern);
+            teamArray.push(intern)
+            createTeam();
+        });
+    }
+
+    createEngineer = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the Engineer's name",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a name."
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the Engineer's id?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an ID."
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the Engineer's email?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter an email."
+                }
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is the Engineer's GitHub username?",
+                validate: data => {
+                    if(data !== ""){
+                        return true
+                    } 
+                    return "Please enter a username."
+                }
+            },
+
+        ]).then((data) => {
+            const intern = new intern(
+                data.engineerName, 
+                data.engineerId,
+                data.engineerEmail,
+                data.engineerGithub,
+                );
+            console.log(intern);
+            teamArray.push(intern)
+            createTeam();
+        });
+    }
+
+    createEngineer();
+    createIntern();
+    createEmployee();
+    createManager();
+
+}
+
+
+menu();
