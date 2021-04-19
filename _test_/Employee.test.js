036d9d8e2fcc839f7127916ed893ceb89f398fde
Adam Alcantara <adamjalcantara@gmail.com>
2021-04-18 20:28:34 -0400

    Added employee test

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -2,12 +2,36 @@
 const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employees");
 
-test ("Can Instantiate Employee", () => {
-    const employee = new Employee();
-    expect(typeof(employee)).toBe("object");
-})
+describe("Employee", () => {
+    describe("initial", () => {
+        it("Should create an object with employee name, id, and email", () => {
+            const employee = new Employee("Employee", 123, "employee@job.com");
+            expect(employee).toEqual({name: "Employee", id: 123, email: "employee@job.com"});
+        });
+    });
 
-test ("Can set name through constructor arguments", () => {
-    const employee = new Employee("Patrick");
-    expect(employee.name).toBe("Patrick");
+    test ("Can Instantiate Employee", () => {
+        const employee = new Employee();
+        expect(typeof(employee)).toBe("object");
+    })
+
+    test ("can get name with getName function", () => {
+        const employee = new Employee("employee");
+        expect(employee.getName()).toBe(employee.name);
+    })
+
+    test ("can get id with getID function", () => {
+        const employee = new Employee(123);
+        expect(employee.getId()).toBe(employee.id);
+    })
+
+    test ("can get email with getEmail function", () => {
+        const employee = new Employee("employee@job.com");
+        expect(employee.getEmail()).toBe(employee.email);
+    })
+
+    test ("can get role with getRole function", () => {
+        const employee = new Employee("Employee");
+        expect(employee.getRole()).toBe("Employee");
+    })
 })
